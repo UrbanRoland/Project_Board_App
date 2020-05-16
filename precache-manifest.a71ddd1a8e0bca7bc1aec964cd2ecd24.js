30f7dc9a24c7d06a93da2139c9886556cf18aa17
Urban Roland <h776775@stud.u-szeged.hu>
2020-05-16 22:01:13 +0200

    Updates

---
+++ precache-manifest.a71ddd1a8e0bca7bc1aec964cd2ecd24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4befb73fffaf6bd77ad8beb544c9e9",
+    "url": "/UrbanRoland/Project_Board_App/index.html"
+  },
+  {
+    "revision": "bcc6d71a402b7e8e728b",
+    "url": "/UrbanRoland/Project_Board_App/static/css/2.8f4dfbca.chunk.css"
+  },
+  {
+    "revision": "4fae2ba5c0c66f21f775",
+    "url": "/UrbanRoland/Project_Board_App/static/css/main.5fd6d562.chunk.css"
+  },
+  {
+    "revision": "bcc6d71a402b7e8e728b",
+    "url": "/UrbanRoland/Project_Board_App/static/js/2.d895f448.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/UrbanRoland/Project_Board_App/static/js/2.d895f448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fae2ba5c0c66f21f775",
+    "url": "/UrbanRoland/Project_Board_App/static/js/main.53924b0c.chunk.js"
+  },
+  {
+    "revision": "5b06cfac70c0e6e051d3",
+    "url": "/UrbanRoland/Project_Board_App/static/js/runtime-main.9a9c6740.js"
+  }
+]);
